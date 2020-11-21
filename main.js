6c0dcdc53b9a13010482ff561d6d25347413fea4
advancedbear <advanced.bear@gmail.com>
2020-11-21 23:01:39 +0900

    ログ出力するように変更

--- main.js
+++ main.js
@@ -3,8 +3,24 @@ const LiveChat = require('youtube-chat').LiveChat
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
+const log4js = require('log4js')
 app.use(cors());
 app.use(helmet())
+
+log4js.configure({
+    appenders: {
+        chatlog: { 
+            type: 'dateFile', 
+            filename: 'youtube-chat.log',
+            pattern: "-yyyyMMdd",
+            backups: 14,
+            compress: true}
+    },
+    categories: {
+        default: { appenders: ['chatlog'], level: 'info' },
+    }
+});
+var logger = log4js.getLogger('chatlog');
 
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
@@ -12,13 +28,15 @@ const io = require('socket.io')(http);
 var youtubeChat = {};
 
 io.on('connection', (socket) => {
+    logger.info(socket.id+" is connecting.")
     console.log("Started!", socket.id);
-    socket.on('event', ()=>{})
+    socket.on('event', () => { })
     socket.on('begin', (v) => {
-        if(v.match(/[^"&?\/\s]{11}/)){
+        if (v.match(/[^"&?\/\s]{11}/)) {
+            logger.info(`${socket.id} begin to receive chat. (${v})`)
             console.log('videoId: ' + v);
-            youtubeChat[socket.id] = new LiveChat({liveId: v})
-            youtubeChat[socket.id].on('comment', (msg)=>{
+            youtubeChat[socket.id] = new LiveChat({ liveId: v })
+            youtubeChat[socket.id].on('comment', (msg) => {
                 io.to(socket.id).emit('comment', msg);
             })
             youtubeChat[socket.id].start()
@@ -26,7 +44,8 @@ io.on('connection', (socket) => {
     });
     socket.on('disconnect', () => {
         console.log("Stopped!", socket.id);
-        if(typeof youtubeChat[socket.io] != "undefined"){
+        if (typeof youtubeChat[socket.io] != "undefined") {
+            logger.info(`${socket.id} diconnected.`)
             youtubeChat[socket.id].stop()
             youtubeChat[socket.id] = null;
             console.log(youtubeChat)
